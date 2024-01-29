cab24b1c47318016279b72a6ce3204127e7f095d
Ryoji Kuwae Neto <ryojikn@gmail.com>
2024-01-29 01:15:47 -0300

    Changing minor sizing details

--- src/App.tsx
+++ src/App.tsx
@@ -39,17 +39,17 @@ export function App() {
 
             <aside className="w-80 space-y-6">
                 <form className="space-y-6">
-                    <label htmlFor="video" className="border flex rounded-md aspect-video cursor-pointer border-dashed text-sm flex-col gap-2 items-center justify-center text-muted-foreground hover:bg-primary/5">
+                    <Label htmlFor="video" className="h-24 border flex rounded-md cursor-pointer border-dashed text-sm flex-col gap-2 items-center justify-center text-muted-foreground hover:bg-primary/5">
                         Selecione um video
                         <FileVideo className="w-4 h-4"></FileVideo>
-                    </label>
+                    </Label>
                     <input type="file" id="video" accept="video/mp4" className="sr-only" />
 
                     <Separator />
 
                     <div className="space-y-2">
                         <Label htmlFor="transcription_prompt">Prompt de transcrição</Label>
-                        <Textarea id="transcription_prompt" className="h-20 leading-relaxed resize-none" placeholder="Inclua palavras chave mencionadas no vídeo separadas por vírgula (,)"/>
+                        <Textarea id="transcription_prompt" className="h-10 text-xs leading-relaxed resize-none" placeholder="Inclua palavras chave mencionadas no vídeo separadas por vírgula (,)"/>
                     </div>
                     <Button type="submit" className="w-full">
                         Carregar video
